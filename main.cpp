a58e7ce1172ce9c0579888cfcf4a8dc6b9571b0d
itemqq <itemqq@loccs>
2021-02-24 18:52:58 +0800

    Add enc_req and enc_esp handler

--- main.cpp
+++ main.cpp
@@ -6,20 +6,92 @@
 
 #define m_assert(expr, msg) assert(( (void)(msg), (expr) ))
 
+
+int find_skdm_flag = 0;
+int find_skds_flag = 0;
+unsigned char skdm[8] = { 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+unsigned char skds[8] = { 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+
 void printf_raw_data(int channel,unsigned char *data,int len){
     uint32_t AccessAddress = data[10]|(data[11]<<8)|(data[12]<<16)|(data[13]<<24);
-    std::cout<<"Channel: "<<channel<<"  "<<AccessAddress<<std::endl;
+    printf("Channel: %d, AccessAddress: 0x%x\n",channel,AccessAddress);
     for (int i=0;i<len;++i){
         printf("%x",data[i]);
     }
     std::cout<<std::endl;
 }
 
-void get_raw_data_cb(int dev_index,int channel,unsigned char *data,int data_len){
-    printf_raw_data(channel,data,data_len);
+void check_skd(int channel, unsigned char* data, int data_len)
+{
+    //enc_req
+    if (data[15] == 0x17 && data[16] == 0x03 && data_len == 44){
+        find_skdm_flag = 1;
+        for (int i = 0; i < 8; i++){
+            skdm[i] = data[34 - i];
+        }
+    }
+    //enc_rsp
+    if (data[15] == 0x0d && data[16] == 0x04 && data_len == 34){
+        find_skds_flag = 1;
+        for (int i = 0; i < 8; i++){
+            skds[i] = data[24 - i];
+        }
+    }
+    // Debug
+    if(find_skds_flag&&find_skdm_flag)
+        printf_raw_data(channel,data,data_len);
 }
 
+// Start current trace collection
+bool startCurTr(){
+
+}
+
+// Stop current trace collection
+bool stopCurTr(){
+
+}
+
+// Record the plaintext
+bool recordPt(int num, FILE* fd){
+    fwrite(skdm,1,8*sizeof(unsigned char),fd);
+    fwrite(skds,1,8*sizeof(unsigned char),fd);
+}
+
+void get_raw_data_cb(int dev_index,int channel,unsigned char *data,int data_len){
+    // Debug
+    // printf_raw_data(channel,data,data_len);
+    check_skd(channel,data,data_len);
+}
+
+bool collect(const char ptFileName[],int targetNum){
+    FILE* ptFile=fopen(ptFileName,"wb");
+    int collectedNum=0;
+    //Start first trace collection
+    startCurTr();
+    // Then loop
+    while(true){
+        if(find_skdm_flag&&find_skds_flag){
+            stopCurTr();
+            find_skdm_flag=find_skds_flag=0;
+            recordPt(++collectedNum,ptFile);
+            if(collectedNum>=targetNum){
+                break;
+            }
+            startCurTr();
+        }else{
+            continue;
+        }
+    }
+}
+
+
 int main() {
+    // Collection configs
+    const char* ptFileName="/home/itemqq/Desktop/data/plaintext";
+    const int targetNum=10;
+
+
     // Open devices
     int ret=scan_dev(NULL), DevIndex=0;
     m_assert(ret>0,"scan_dev <= 0");
@@ -28,7 +100,9 @@ int main() {
 
     // Get data will work in new thread
     start_get_data(DevIndex,get_raw_data_cb);
-    sleep(10);
+
+    // Collection
+    collect(ptFileName,targetNum);
 
     // Close device
     ret=close_dev(DevIndex);
